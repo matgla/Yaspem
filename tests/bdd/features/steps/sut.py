3cba4d6238139656a0ac8884bde8cfcaea4539fe
Mateusz Stadnik <matgla@live.com>
2023-01-11 09:44:55 +0100

    debug test

--- tests/bdd/features/steps/sut.py
+++ tests/bdd/features/steps/sut.py
@@ -24,6 +24,7 @@ def execute_sut(args = []):
     yaspem_path = root_dir / "yaspem.py"
     args_to_run = [python_path.absolute(), yaspem_path]
     args_to_run.extend(args)
+    assert False, "yy: " + str(args_to_run)
     return run(args_to_run, capture_output=True, cwd=working_dir.absolute())
 
 @given('we have YASPEM executable')
