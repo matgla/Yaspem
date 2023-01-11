0791e985b8f5fabe2a6d9b02c78ef2190b666438
Mateusz Stadnik <matgla@live.com>
2023-01-11 09:24:45 +0100

    changed python name

--- run_bdd.sh
+++ run_bdd.sh
@@ -2,12 +2,9 @@
 
 case "$(uname -sr)" in
     CYGWIN*|MINGW*|MINGW32*|MSYS*)
-        python_exec=test_env/Scripts/python3.exe;;
+        python_exec=test_env/Scripts/python.exe;;
     *)
         python_exec=test_env/bin/python3;;
 esac
 
-pwd;
-ls test_env;
-ls test_env/Scripts;
 cd tests/bdd && ../../$python_exec -m behave;
